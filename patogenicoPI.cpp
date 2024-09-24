e01d7f25509ed8d8d672d1f10fbb59c88fabfa67
jonasbroder <jp.rcha@gmail.com>
2024-09-24 04:32:08 -0300

    tentando resolver bugs; enviando pra poder testar em outros computadores

--- patogenicoPI.cpp
+++ patogenicoPI.cpp
@@ -18,10 +18,12 @@ bool isColliding(int x1, int y1, int width1, int height1, int x2, int y2, int wi
 }
 
 // Cores
-ALLEGRO_COLOR RED = al_map_rgb(255, 0, 0);
-ALLEGRO_COLOR GREEN = al_map_rgb(0, 255, 0);
 ALLEGRO_COLOR BLACK = al_map_rgb(0, 0, 0);
 ALLEGRO_COLOR WHITE = al_map_rgb(255, 255, 255);
+ALLEGRO_COLOR RED = al_map_rgb(255, 0, 0);
+ALLEGRO_COLOR GREEN = al_map_rgb(0, 255, 0);
+ALLEGRO_COLOR BLUE = al_map_rgb(0, 0, 255);
+
 
 
 
@@ -56,8 +58,8 @@ int main() {
 
 
     /*****VARIAVEIS LOCAIS*****/
-    float circle_x = displayWidth/2;  // Posição inicial do círculo (centro da tela)
-    float circle_y = displayHeight/2;
+    float circle_x = displayWidth / 2;  // Posição inicial do círculo (centro da tela)
+    float circle_y = displayHeight / 2;
     int CIRCUNFERENCIA = 30;         //como a gente quer que a circunferencia do player aumente, eu mudei de const float pra só int    -joao
     const float MOVE_VELO = 5.0;     // Velocidade do movimento do círculo
 
@@ -76,7 +78,7 @@ int main() {
     for (int i = 0; i < 30; i++) {
         do {
             bolinhas[i].x = rand() % displayWidth;
-        } while (bolinhas[i].x > displayWidth + bolinhas[0].r || bolinhas[i].x < 0-bolinhas[0].r); //garante que a bolinha vai ficar inteiramente dentro da tela
+        } while (bolinhas[i].x > displayWidth + bolinhas[0].r || bolinhas[i].x < 0 - bolinhas[0].r); //garante que a bolinha vai ficar inteiramente dentro da tela
         do {
             bolinhas[i].y = rand() % displayHeight;
         } while (bolinhas[i].y > displayHeight + bolinhas[0].r || bolinhas[i].y < 0 - bolinhas[0].r);
@@ -115,30 +117,37 @@ int main() {
                 circle_x += move_x;
                 circle_y += move_y;
             }
-
-            // Define a cor de fundo (preto neste caso)
+            
+            // Define a cor de fundo (BRANCO neste caso)
             al_clear_to_color(WHITE);
+
+            // Desenha o círculo na nova posição
+            al_draw_filled_circle(circle_x, circle_y, CIRCUNFERENCIA, RED);
+
+            //circulo de debug
+            al_draw_filled_circle(circle_x, circle_y, CIRCUNFERENCIA/2, BLUE);
+
+            //desenha bolinhas
             for (int i = 0; i < 30; i++) {
                 al_draw_filled_circle(bolinhas[i].x, bolinhas[i].y, bolinhas[0].r * 2, GREEN);
             }
 
-            // Desenha o círculo na nova posição
-            al_draw_filled_circle(circle_x, circle_y, CIRCUNFERENCIA, RED);
+            
 
             //calcula colisão entre player-bolinhas
                 //código não muito eficiente (mas funcional!!!), é provisório e deve ser consertado com urgência    -joao
             for (int i = 0; i < 30; i++) {
                 if (isColliding(bolinhas[i].x - bolinhas[0].r, bolinhas[i].y - bolinhas[0].r, bolinhas[0].r, bolinhas[0].r,   //"se as coordenadas de cada bolinha...
-                                circle_x - CIRCUNFERENCIA / 2, circle_y - CIRCUNFERENCIA / 2, CIRCUNFERENCIA, CIRCUNFERENCIA))//...estiver dentro das coordenadas do player
+                    circle_x - CIRCUNFERENCIA/2, circle_y - CIRCUNFERENCIA/2, CIRCUNFERENCIA, CIRCUNFERENCIA))//...estiver dentro das coordenadas do player
                 {                                                                                                             //então faça isso:"
-                    CIRCUNFERENCIA +=2; //aumenta player
+                    CIRCUNFERENCIA += 2; //aumenta player
                     do {
                         bolinhas[i].x = rand() % displayWidth; //após "comida", randomiza novamente a posição da bolinha 
                     } while (bolinhas[i].x > displayWidth + bolinhas[0].r || bolinhas[i].x < 0 - bolinhas[0].r); //garante que a bolinha vai ficar inteiramente dentro da tela
                     do {
                         bolinhas[i].y = rand() % displayHeight;
                     } while (bolinhas[i].y > displayHeight + bolinhas[0].r || bolinhas[i].y < 0 - bolinhas[0].r);
-                    printf("colidiu!!\n");
+                    printf("colidiu!!   tamanho da circunferencia: %d\n", CIRCUNFERENCIA);
                 }
             }
             // Atualiza a tela

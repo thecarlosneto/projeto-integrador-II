83bd091cdb18d14b191aa0e296c2115e8e0a9d4c
jonasbroder <jp.rcha@gmail.com>
2024-10-01 18:01:55 -0300

    atualização da colisão e desenho da grade de fundo

--- patogenicoPI.cpp
+++ patogenicoPI.cpp
@@ -12,30 +12,27 @@ e sua largura e altura. Se os valores da instância 1 estiverem dentro da instâ
 (estão colidindo), caso contrário retorna falso (não houve colisão).
 */
 bool isColliding(int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2) {
-    if ((x2 < x1 && x1 < x2 + width2) && (y2 < y1 && y1 < y2 + height2))
+    if ((x2 < x1 && x1 < x2 + width2) && (y2 < y1 && y1 < y2 + height2)
+        &&
+        (x2 < x1 + width1 && x1 + width1 < x2 + width2) && (y2 < y1 + height1 && y1 + height1 < y2 + height2))
         return true;
     return false;
 }
 
-// Cores
-ALLEGRO_COLOR BLACK = al_map_rgb(0, 0, 0);
-ALLEGRO_COLOR WHITE = al_map_rgb(255, 255, 255);
-ALLEGRO_COLOR RED = al_map_rgb(255, 0, 0);
-ALLEGRO_COLOR GREEN = al_map_rgb(0, 255, 0);
-ALLEGRO_COLOR BLUE = al_map_rgb(0, 0, 255);
-
-
-
-
-
 int main() {
 
+    ALLEGRO_COLOR BLACK = al_map_rgb(0, 0, 0);
+    ALLEGRO_COLOR WHITE = al_map_rgb(255, 255, 255);
+    ALLEGRO_COLOR GRAY = al_map_rgb(200, 200, 200);
+    ALLEGRO_COLOR RED = al_map_rgb(255, 0, 0);
+    ALLEGRO_COLOR GREEN = al_map_rgb(0, 255, 0);
+    ALLEGRO_COLOR BLUE = al_map_rgb(0, 0, 255);
+    ALLEGRO_COLOR PINK = al_map_rgb(221, 160, 221);
 
     /*****SETUP DO ALLEGRO*****/
     al_init();                  //para inicializar
     al_init_primitives_addon(); //para desenhar figuras
     al_install_mouse();         //para detectar mouse
-
 
     // Cria uma janela
     int displayWidth = 800;
@@ -60,11 +57,11 @@ int main() {
     /*****VARIAVEIS LOCAIS*****/
     float circle_x = displayWidth / 2;  // Posição inicial do círculo (centro da tela)
     float circle_y = displayHeight / 2;
-    int CIRCUNFERENCIA = 30;         //como a gente quer que a circunferencia do player aumente, eu mudei de const float pra só int    -joao
-    const float MOVE_VELO = 5.0;     // Velocidade do movimento do círculo
-
+    int circle_r = 15;                  // Tamanho inicial do círculo
+    const float MOVE_VELO = 5.0;        // Velocidade do movimento do círculo
+    const int espacoEntreLinhas = 50;
     // bolinhas que são comidas pro player crescer
-    struct bolinha {
+     struct bolinha {
         int x;
         int y;
         int r = 5; //raio
@@ -83,7 +80,6 @@ int main() {
             bolinhas[i].y = rand() % displayHeight;
         } while (bolinhas[i].y > displayHeight + bolinhas[0].r || bolinhas[i].y < 0 - bolinhas[0].r);
     }
-
 
     while (true) {
         ALLEGRO_EVENT event;
@@ -118,38 +114,47 @@ int main() {
                 circle_y += move_y;
             }
             
+
+            /*****DESENHO*****/
+
             // Define a cor de fundo (BRANCO neste caso)
             al_clear_to_color(WHITE);
 
+            // Desenha a grade de fundo
+            for (int i = 1; i < displayWidth / espacoEntreLinhas; i++) {
+                al_draw_line(i * espacoEntreLinhas, 0, i * espacoEntreLinhas, 600, GRAY, 2);     //desenha a linha
+                for (int j = 1; j < displayHeight / espacoEntreLinhas; j++) {
+                    al_draw_line(0 ,j * espacoEntreLinhas, 800, j * espacoEntreLinhas, GRAY, 2); //desenha a coluna
+                }
+            }
             // Desenha o círculo na nova posição
-            al_draw_filled_circle(circle_x, circle_y, CIRCUNFERENCIA, RED);
+            al_draw_filled_circle(circle_x, circle_y, circle_r, RED);
 
-            //circulo de debug
-            al_draw_filled_circle(circle_x, circle_y, CIRCUNFERENCIA/2, BLUE);
 
             //desenha bolinhas
             for (int i = 0; i < 30; i++) {
                 al_draw_filled_circle(bolinhas[i].x, bolinhas[i].y, bolinhas[0].r * 2, GREEN);
             }
-
             
-
             //calcula colisão entre player-bolinhas
                 //código não muito eficiente (mas funcional!!!), é provisório e deve ser consertado com urgência    -joao
             for (int i = 0; i < 30; i++) {
-                if (isColliding(bolinhas[i].x - bolinhas[0].r, bolinhas[i].y - bolinhas[0].r, bolinhas[0].r, bolinhas[0].r,   //"se as coordenadas de cada bolinha...
-                    circle_x - CIRCUNFERENCIA/2, circle_y - CIRCUNFERENCIA/2, CIRCUNFERENCIA, CIRCUNFERENCIA))//...estiver dentro das coordenadas do player
-                {                                                                                                             //então faça isso:"
-                    CIRCUNFERENCIA += 2; //aumenta player
+                if (isColliding(bolinhas[i].x-bolinhas[i].r, bolinhas[i].y - bolinhas[i].r, bolinhas[i].r * 2, bolinhas[i].r * 2, //se as coordenadas das bolinhas ....
+                                circle_x-circle_r + bolinhas[i].r, circle_y-circle_r + bolinhas[i].r, circle_r*2 - bolinhas[i].r, circle_r*2 - bolinhas[i].r))                                    //...estiverem dentro das coordenadas do player
+                {
+                    circle_r += 2; //aumenta player
                     do {
                         bolinhas[i].x = rand() % displayWidth; //após "comida", randomiza novamente a posição da bolinha 
                     } while (bolinhas[i].x > displayWidth + bolinhas[0].r || bolinhas[i].x < 0 - bolinhas[0].r); //garante que a bolinha vai ficar inteiramente dentro da tela
                     do {
                         bolinhas[i].y = rand() % displayHeight;
                     } while (bolinhas[i].y > displayHeight + bolinhas[0].r || bolinhas[i].y < 0 - bolinhas[0].r);
-                    printf("colidiu!!   tamanho da circunferencia: %d\n", CIRCUNFERENCIA);
+                    printf("colidiu!!\n");
                 }
             }
+            //reseta tamanho do player
+            if (circle_r >= 50)
+                circle_r = 20;
             // Atualiza a tela
             al_flip_display();
         }

eafae5b58dda935542bcd1405d1875a6c301967f
jonasbroder <jp.rcha@gmail.com>
2024-10-03 18:00:26 -0300

    função para randomizar bolinhas feita

--- patogenicoPI.cpp
+++ patogenicoPI.cpp
@@ -1,23 +1,54 @@
+/*
+TO-DO
+
+atribuir um espaço na grid p cada bolinha
+
+incrementar o numero de bolinhas naquela celula da grid
+
+checar a colisao p cada bolinha dentro das 9 celulas em volta do player
+
+*/
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h> // Para criar um circulo
 #include <cmath> // Para utilizar o sqrt
 #include <stdlib.h>
 #include <stdio.h>
 
+#define displayWidth 800
+#define displayHeight 600
+
+// bolinhas que são comidas pro player crescer
+typedef struct {
+    int x;
+    int y;
+}bolinha;
 
 /*
-função *PROVISÓRIA* pra calcular colisão.
+função pra calcular colisão. (mais especificamente quando um objeto está INTEIRAMENTE dentro de outro)
 recebe as coordenadas de duas instâncias (inserir coord A PARTIR DO CANTO SUPERIOR ESQUERDO, igual no p5js)
 e sua largura e altura. Se os valores da instância 1 estiverem dentro da instância 2, retorna verdadeiro
 (estão colidindo), caso contrário retorna falso (não houve colisão).
 */
-bool isColliding(int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2) {
+bool isInside(int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2) {
     if ((x2 < x1 && x1 < x2 + width2) && (y2 < y1 && y1 < y2 + height2)
         &&
         (x2 < x1 + width1 && x1 + width1 < x2 + width2) && (y2 < y1 + height1 && y1 + height1 < y2 + height2))
         return true;
     return false;
 }
+
+void randomizarBolinhas(bolinha* pBolinhas, const int* praioBolinha, const int* pnumBolinhas, int i) {
+        do {
+            pBolinhas[i].x = rand() % displayWidth;
+        } while (pBolinhas[i].x > displayWidth + *praioBolinha || pBolinhas[i].x < 0 - *praioBolinha); //garante que a bolinha vai ficar inteiramente dentro da tela
+        do {
+            pBolinhas[i].y = rand() % displayHeight;
+        } while (pBolinhas[i].y > displayHeight + *praioBolinha || pBolinhas[i].y < 0 - *praioBolinha);
+}
+
+//randomizarBolinhas(&raioBolinha);
+
 
 int main() {
 
@@ -35,8 +66,6 @@ int main() {
     al_install_mouse();         //para detectar mouse
 
     // Cria uma janela
-    int displayWidth = 800;
-    int displayHeight = 600;
     ALLEGRO_DISPLAY* display = al_create_display(displayWidth, displayHeight);
 
     // define o FPS
@@ -59,28 +88,21 @@ int main() {
     float circle_y = displayHeight / 2;
     int circle_r = 15;                  // Tamanho inicial do círculo
     const float MOVE_VELO = 5.0;        // Velocidade do movimento do círculo
-    const int espacoEntreLinhas = 50;
-    // bolinhas que são comidas pro player crescer
-     struct bolinha {
-        int x;
-        int y;
-        int r = 5; //raio
-    };
 
+    const int numBolinhas = 100;    //quantas bolinhas há no mapa
+    const int raioBolinha = 5;
+
+    #define espacoEntreLinhas 50
+    #define nCelulasGrid (displayWidth / espacoEntreLinhas) * (displayHeight / espacoEntreLinhas)
+    int gridMap[nCelulasGrid][11];     
 
     /*****SETUP DO JOGO*****/
 
     //define um valor aleatório para cada bolinha no mapa
-    bolinha bolinhas[30];
-    for (int i = 0; i < 30; i++) {
-        do {
-            bolinhas[i].x = rand() % displayWidth;
-        } while (bolinhas[i].x > displayWidth + bolinhas[0].r || bolinhas[i].x < 0 - bolinhas[0].r); //garante que a bolinha vai ficar inteiramente dentro da tela
-        do {
-            bolinhas[i].y = rand() % displayHeight;
-        } while (bolinhas[i].y > displayHeight + bolinhas[0].r || bolinhas[i].y < 0 - bolinhas[0].r);
+    bolinha* bolinhas = (bolinha*)malloc(numBolinhas * sizeof(bolinha));
+    for (int i = 0; i < numBolinhas; i++) {
+        randomizarBolinhas(bolinhas, &raioBolinha, &numBolinhas, i);
     }
-
     while (true) {
         ALLEGRO_EVENT event;
         // Espera por um evento
@@ -127,34 +149,30 @@ int main() {
                     al_draw_line(0 ,j * espacoEntreLinhas, 800, j * espacoEntreLinhas, GRAY, 2); //desenha a coluna
                 }
             }
+           
+            //desenha bolinhas
+            for (int i = 0; i < numBolinhas; i++) {
+                al_draw_filled_circle(bolinhas[i].x, bolinhas[i].y, raioBolinha * 2, GREEN);
+            }
             // Desenha o círculo na nova posição
             al_draw_filled_circle(circle_x, circle_y, circle_r, RED);
 
 
-            //desenha bolinhas
-            for (int i = 0; i < 30; i++) {
-                al_draw_filled_circle(bolinhas[i].x, bolinhas[i].y, bolinhas[0].r * 2, GREEN);
-            }
-            
+
             //calcula colisão entre player-bolinhas
                 //código não muito eficiente (mas funcional!!!), é provisório e deve ser consertado com urgência    -joao
-            for (int i = 0; i < 30; i++) {
-                if (isColliding(bolinhas[i].x-bolinhas[i].r, bolinhas[i].y - bolinhas[i].r, bolinhas[i].r * 2, bolinhas[i].r * 2, //se as coordenadas das bolinhas ....
-                                circle_x-circle_r + bolinhas[i].r, circle_y-circle_r + bolinhas[i].r, circle_r*2 - bolinhas[i].r, circle_r*2 - bolinhas[i].r))                                    //...estiverem dentro das coordenadas do player
+            for (int i = 0; i < numBolinhas; i++) {
+                if (isInside(bolinhas[i].x- raioBolinha, bolinhas[i].y - raioBolinha, raioBolinha * 2, raioBolinha * 2, //se as coordenadas das bolinhas ....
+                                circle_x-circle_r + raioBolinha, circle_y-circle_r + raioBolinha, circle_r*2 - raioBolinha, circle_r*2 - raioBolinha))                                    //...estiverem dentro das coordenadas do player
                 {
-                    circle_r += 2; //aumenta player
-                    do {
-                        bolinhas[i].x = rand() % displayWidth; //após "comida", randomiza novamente a posição da bolinha 
-                    } while (bolinhas[i].x > displayWidth + bolinhas[0].r || bolinhas[i].x < 0 - bolinhas[0].r); //garante que a bolinha vai ficar inteiramente dentro da tela
-                    do {
-                        bolinhas[i].y = rand() % displayHeight;
-                    } while (bolinhas[i].y > displayHeight + bolinhas[0].r || bolinhas[i].y < 0 - bolinhas[0].r);
-                    printf("colidiu!!\n");
+                    circle_r ++; //aumenta player
+                    randomizarBolinhas(bolinhas, &raioBolinha, &numBolinhas, i);
                 }
             }
             //reseta tamanho do player
-            if (circle_r >= 50)
+            if (circle_r >= 75) {
                 circle_r = 20;
+            }
             // Atualiza a tela
             al_flip_display();
         }
@@ -165,5 +183,6 @@ int main() {
     al_destroy_event_queue(event_queue);
     al_destroy_timer(timer);
 
+    free(bolinhas);
     return 0;
 }

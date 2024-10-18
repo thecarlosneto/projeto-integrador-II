b65bfffb152676d85051618c437aa4e9e2322750
Vick <vickkathlyn@outlook.com>
2024-10-18 11:43:35 -0300

    cronometro e etc

--- patogenicoPI.cpp
+++ patogenicoPI.cpp
@@ -6,24 +6,81 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
 
+int teste;
 int tela = 1;
+int width = 800;
+int height = 600;
+int cron = 20;
+int cronP = 20; // para o cronometro do viremia 
+char cron_str[10];
 
-bool isColliding(int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2) {
-    if ((x2 < x1 && x1 < x2 + width2) && (y2 < y1 && y1 < y2 + height2))
-        return true;
-    return false;
+#define gameOver 0
+#define telaInicial 1
+#define seletorFase 2
+#define ataqueMosquito 3
+#define fagocitose 4
+#define viremia 5
+
+
+/*Função que gera coordenadas de X aleatorias com base no tamanho do display,
+  preenchendo um array. Onde a primeira e a última posição já tem coordenadas
+  pré estabelecidas*/
+void geracoordenadasX(int* coordenadaX, int tamanho, int x1, int x2) {
+    coordenadaX[0] = x1;
+    coordenadaX[tamanho - 1] = x2;
+    for (int i = 1; i < tamanho - 1; i++) {
+        coordenadaX[i] = 20 + rand() % (width - 40);
+    }
+}
+
+/*Função que gera coordenadas de Y aleatorias com base no tamanho do display,
+  preenchendo um array. Onde a primeira e a última posição já tem coordenadas
+  pré estabelecidas*/
+void geracoordenadasY(int* coordenadaY, int tamanho, int y1, int y2) {
+    coordenadaY[0] = y1;
+    coordenadaY[tamanho - 1] = y2;
+    for (int i = 1; i < tamanho - 1; i++) {
+        coordenadaY[i] = 20 + rand() % (height - 40);
+    }
+}
+
+//Função que gera retas colineares de acordo com as coordenadas 
+void gerarLinhas(int* coordenadaX, int* coordenadaY, int tamanho, int espessuraLinha) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        al_draw_line(coordenadaX[i], coordenadaY[i], coordenadaX[i + 1], coordenadaY[i + 1], al_map_rgb(119, 25, 15), espessuraLinha);
+    };
+}
+
+// Função para verificar se o círculo está sobre uma linha com espessura
+bool foraDaLinha(float x, float y, float x1, float y1, float x2, float y2, float espessura) {
+    // Calcular a distância perpendicular do ponto à reta
+    float A = y2 - y1;
+    float B = x1 - x2;
+    float C = x2 * y1 - x1 * y2;
+    float distancia = fabs(A * x + B * y + C) / sqrt(A * A + B * B);
+
+    return distancia > espessura / 2;
+}
+
+#define PI 3.14159265358979323846
+//função que cria linhas onduladas por meio do calculo da Função Seno
+void linhasOnduladas(float x1, float y1, float x2, float y2, int qtdOndas) {
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float comprimentoDaOnda = dx / qtdOndas;
+    float amplitude = 20; // Ajuste a amplitude para controlar a altura das ondas
+
+    for (int i = 0; i <= qtdOndas; ++i) {
+        float x = x1 + i * comprimentoDaOnda;
+        float y = y1 + dy * i / qtdOndas + amplitude * sin(i * 2 * PI / qtdOndas);
+        al_draw_line(x, y, x + comprimentoDaOnda, y, al_map_rgb(252, 186, 3), 5.0);
+    }
 }
 
 
 
-const float CIRCUFERENCIA = 50;
-const float MOVE_VELO = 5.0; // Velocidade do movimento do círculo
-
-// Posição inicial do círculo (centro da tela)
-float circle_x = 400;
-float circle_y = 300;
-
 int main() {
+    srand(time(NULL)); //inicializa a semente para números aleatórios
 
     // Inicializa o Allegro
     al_init();
@@ -44,12 +101,16 @@ int main() {
     // 60 fps
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
 
+    ALLEGRO_TIMER* countdown_timer = al_create_timer(1.0);
+
     // Criação de uma janela de 640x480 pixels
-    ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+    ALLEGRO_DISPLAY* display = al_create_display(width, height);
 
     // Carregar uma imagem (substitua "sua_imagem.png" pelo caminho da sua imagem)
     ALLEGRO_BITMAP* background = al_load_bitmap("img/telaTeste.png");
     ALLEGRO_BITMAP* background2 = al_load_bitmap("img/tela2Teste.png");
+    ALLEGRO_BITMAP* backgroundViremia = al_load_bitmap("img/backgroundViremia.png");
+    ALLEGRO_BITMAP* virusViremia = al_load_bitmap("img/virus.png");
 
     if (!display) {
         printf("Erro ao criar a janela.\n");
@@ -68,6 +129,10 @@ int main() {
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_register_event_source(event_queue, al_get_timer_event_source(timer));
     al_register_event_source(event_queue, al_get_mouse_event_source());
+    al_register_event_source(event_queue, al_get_timer_event_source(countdown_timer));
+
+
+    al_start_timer(countdown_timer); // Inicia o timer para o cronômetro
 
     // Limpa a tela com uma cor de fundo
     al_clear_to_color(al_map_rgb(0, 0, 0));
@@ -78,6 +143,44 @@ int main() {
 
     // Inicia o temporizador
     al_start_timer(timer);
+
+    // - - - - - - - VARIÁVEIS PARA VIREMIA - - - - - - -
+    //Coordenadas pré estabelecidas 
+    int x1 = 50, y1 = 535;
+    int x2 = 740, y2 = 50;
+
+    // Posição inicial do círculo
+    int circle_x = x1 + 10;
+    int circle_y = y1 + 10;
+
+    bool startJogo = false;
+
+    int tamanho = 4;
+
+    int espessuraLinha = 15.0;
+    bool dentroDaLinha = false;
+    // Aloca memória para o coordenadaX
+    int* coordenadaX = (int*)malloc(tamanho * sizeof(int));
+    if (coordenadaX == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1; // Sai em caso de falha na alocação
+    }
+
+    // Aloca memória para o coordenadaY
+    int* coordenadaY = (int*)malloc(tamanho * sizeof(int));
+    if (coordenadaY == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1; // Sai em caso de falha na alocação
+    }
+
+    // Chama a função para preencher o array com números aleatórios
+    geracoordenadasX(coordenadaX, tamanho, x1, x2);
+    geracoordenadasY(coordenadaY, tamanho, y1, y2);
+
+    // Variáveis para o movimento circular
+    float angle = 0; //variável de ângulo será incrementada a cada frame para simular o movimento circular
+    float radius = 50;
+    // - - - - - - -FIM DAS VARIÁVEIS PARA VIREMIA - - - - - - -
 
     // Loop de eventos
     bool running = true;
@@ -91,10 +194,11 @@ int main() {
         }
 
         switch (tela) {
-        case 1:
+        case telaInicial:
 
             // Desenha a imagem de fundo na tela (na posição (0, 0))
             al_draw_bitmap(background, 0, 0, 0);
+
 
             if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
                 if (ev.mouse.button == 1) { // Botão esquerdo do mouse
@@ -107,36 +211,36 @@ int main() {
                 }
             }
             // Desenhar o texto na tela usando a fonte embutida
-            al_draw_text(font, al_map_rgb(0, 100, 0), 600, 300,ALLEGRO_ALIGN_CENTER, "jogar");
+            al_draw_text(font, al_map_rgb(0, 100, 0), 600, 300, ALLEGRO_ALIGN_CENTER, "jogar");
 
             // Atualiza o display para mostrar o texto
             al_flip_display();
 
             break;
 
-        case 2:
+        case seletorFase:
 
             al_draw_bitmap(background2, 0, 0, 0);
             // Desenhar o texto na tela usando a fonte embutida
-           
+
             if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
                 if (ev.mouse.button == 1) { // Botão esquerdo do mouse
 
-                    if (ev.mouse.y>240 && ev.mouse.y < 340) {
+                    if (ev.mouse.y > 240 && ev.mouse.y < 340) {
 
                         if (ev.mouse.x > 150 && ev.mouse.x < 230) {
 
-                            tela = 3;
+                            tela = ataqueMosquito;
                         }
 
                         if (ev.mouse.x > 345 && ev.mouse.x < 437) {
 
-                            tela = 3;
+                            tela = fagocitose;
                         }
 
                         if (ev.mouse.x > 544 && ev.mouse.x < 627) {
 
-                            tela = 3;
+                            tela = viremia;
                         }
                     }
 
@@ -148,51 +252,120 @@ int main() {
             al_flip_display();
             break;
 
-        case 3:
-
-
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-
-            // Cor do círculo
-            ALLEGRO_COLOR circle_color = al_map_rgb(255, 0, 0);
-
-
-            
-
+        case viremia:
+            // Desenha a imagem de fundo
+            al_draw_bitmap(backgroundViremia, 0, 0, 0);
 
             // Verifica se o evento é do temporizador
             if (ev.type == ALLEGRO_EVENT_TIMER) {
+
+
+                if (ev.type == ALLEGRO_EVENT_TIMER && ev.timer.source == countdown_timer) {
+                    if (cron > 0) {
+                        cron = cron - 1;  // Decrementa o cronômetro
+                    }
+                }
+
+                if (cron == 0) {
+                    al_draw_text(font, al_map_rgb(255, 255, 255), 100, 200, ALLEGRO_ALIGN_CENTER, "GAME OVER");
+                    cron = cronP;
+                    circle_x = x1 + 10;
+                    circle_y = y1 + 10;
+                }
+
                 // Estado do mouse
                 ALLEGRO_MOUSE_STATE mState;
                 al_get_mouse_state(&mState);
 
-                // Calcula a direção e a distância até a posição do mouse
-                // Diferença na coordenada x entre o círculo e o mouse (mState.x - circle_x)
-                float dx = mState.x - circle_x;
-                // Diferença na coordenada y entre o círculo e o mouse (mState.y - circle_y).
-                float dy = mState.y - circle_y;
-                // Distância entre o círculo e o mouse, calculada como a raiz quadrada da soma dos quadrados de dx e dy
-                float distancia = sqrt(dx * dx + dy * dy);
+                // Verifica se o mouse está sobre o círculo
+                if (mState.x >= circle_x - 10 && mState.x <= circle_x + 10 &&
+                    mState.y >= circle_y - 10 && mState.y <= circle_y + 10) {
 
-                // Move o círculo em direção ao mouse
-                // apenas se a distancia > velocidade do circulo
-                if (distancia > MOVE_VELO) {
-                    float move_x = (dx / distancia) * MOVE_VELO;
-                    float move_y = (dy / distancia) * MOVE_VELO;
-                    circle_x += move_x;
-                    circle_y += move_y;
+                    startJogo = true;
                 }
+                else {
+                    startJogo = false;
+                }
+                // Reiniciar a cada iteração
+                dentroDaLinha = false;
 
-                // Define a cor de fundo (preto neste caso)
-                al_clear_to_color(al_map_rgb(0, 0, 0));
+                //Fase viremia começou
+                if (startJogo == true) {
+                    circle_x = mState.x;
+                    circle_y = mState.y;
+                    //Verifica se o círculo está em cima de alguma linha ou dos quadrados brancos
+                    for (int i = 0; i < tamanho - 1; i++) {
+                        if (!foraDaLinha(circle_x, circle_y, coordenadaX[i], coordenadaY[i], coordenadaX[i + 1],
+                            coordenadaY[i + 1], espessuraLinha)
+                            || circle_x >= x1 && circle_x <= x1 + 20 && circle_y >= y1 && circle_y <= y1 + 20
+                            || circle_x >= x2 && circle_x <= x2 + 20 && circle_y >= y2 && circle_y <= y2 + 20) {
+                            dentroDaLinha = true;
+                            break; // Se o cursor estiver sobre uma linha, não precisa verificar as demais
+                        }
+                    }
+
+                    //Fora da linha
+                    if (!dentroDaLinha) {
+                        //deixar comentado por enquanto => tela = gameOver;
+                        al_draw_text(font, al_map_rgb(255, 255, 255), 100, 200, ALLEGRO_ALIGN_CENTER, "GAME OVER");
+                        cron = cronP;
+
+
+                        circle_x = x1 + 10;
+                        circle_y = y1 + 10;
+                    }
+
+                }
+                //Desenha as linhas chamando a função
+                gerarLinhas(coordenadaX, coordenadaY, tamanho, espessuraLinha);
+
+                //Desenha os quadrados brancos
+                al_draw_filled_rectangle(x1, y1, x1 + 20, y1 + 20, al_map_rgb(255, 255, 255));
+                al_draw_filled_rectangle(x2, y2, x2 + 20, y2 + 20, al_map_rgb(255, 255, 255));
+
+                // Calcula a posição da imagem no círculo
+                float x = circle_x + radius * cos(angle);
+                float y = circle_y + radius * sin(angle);
+
+                //Desenha as linhas onduladas a partir da Função seno
+                linhasOnduladas(circle_x, circle_y, x, y, 40);
+
+                // Desenha a imagem
+                al_draw_bitmap(virusViremia, x - al_get_bitmap_width(virusViremia) / 2, y - al_get_bitmap_height(virusViremia) / 2, 0);
 
                 // Desenha o círculo na nova posição
-                al_draw_filled_circle(circle_x, circle_y, CIRCUFERENCIA, circle_color);
+                al_draw_filled_circle(circle_x, circle_y, 10, al_map_rgb(255, 0, 0));
+
+                // Incrementa o ângulo
+                angle += 0.03;
+
+                al_wait_for_event(event_queue, &ev);
+
+
+
+
+                sprintf_s(cron_str, "%d", cron);
+
+                al_draw_text(font, al_map_rgb(255, 255, 255), 100, 100, ALLEGRO_ALIGN_CENTER, cron_str);
+
+                al_draw_text(font, al_map_rgb(255, 255, 255), 50, 50, ALLEGRO_ALIGN_CENTER, "VOLTAR");
+
+                if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+                    if (ev.mouse.button == 1) { // Botão esquerdo do mouse
+
+                        if (ev.mouse.x > 25 && ev.mouse.x < 75 && ev.mouse.y>45 && ev.mouse.y < 60) {
+                            tela = 2;
+                        }
+
+                        printf("Clique detectado na coordenada (%d, %d)\n", ev.mouse.x, ev.mouse.y);
+                    }
+                }
+
 
                 // Atualiza a tela
                 al_flip_display();
             }
-       
+
             break;
 
             // Você pode ter quantos casos quiser
@@ -208,8 +381,13 @@ int main() {
     al_destroy_event_queue(event_queue);
     al_destroy_display(display);
     al_destroy_timer(timer);
-    al_destroy_bitmap(background); 
+    al_destroy_bitmap(background);
     al_destroy_bitmap(background2);
+    al_destroy_bitmap(backgroundViremia);
+
+    // Libera a memória alocada
+    free(coordenadaX);
+    free(coordenadaY);
 
     return 0; // Encerrar o programa corretamente
 }
